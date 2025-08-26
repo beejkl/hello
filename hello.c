b7eaa78509f2b096587aa5523ffa13ecbf21cac6
Blake Landrum <ironmanbee17@gmail.com>
2025-08-26 11:43:48 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Blake Landrum
+ * Date: 2025/8/26
+ *
+ * A simple hello world program in C
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    printf("Hello World!\n");
+    printf("My name is Blake Landrum");
+
+    return 0;
+}
